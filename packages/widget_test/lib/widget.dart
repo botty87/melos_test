098911179c6cc2d1c55b09403094ddacc56eee29
marco bottichio <marco.bottichio@pm.me>
2024-10-24 16:52:11 +0200

    Initial commit

---
+++ packages/widget_test/lib/widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WidgetTest extends StatelessWidget {
+  const WidgetTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('WidgetTest 1');
+  }
+}
